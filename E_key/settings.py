4d9b5996ddc4152b2001ca66888c2cb38bc48f80
fe-iron <elahifaiz00@gmail.com>
2020-06-08 15:24:06 +0530

    addition of whitenoise in requirement file

--- E_key/settings.py
+++ E_key/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #simplified static file serving
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'E_key.urls'
