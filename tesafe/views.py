0c460d0c0c106372b3b4abefa8ae5dd3aadc8abc
fe-iron <elahifaiz00@gmail.com>
2020-09-19 16:21:21 +0530

    change

--- tesafe/views.py
+++ tesafe/views.py
@@ -9,7 +9,6 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 
-
 # dictionary of num to text
 num = {
     1:"One",2:"Two",3:"Three",4:"Four",5:"Five",6:"Six",7:"Seven",8:"Eight",9:"Nine",10:"Ten",11:"Eleven",12:"Twelve",13:"Thirteen",14:"Fourteen",15:"Fifteen",16:"Sixteen",17:"Seventeen",18:"Eighteen",19:"Nineteen",20:"Twenty"
